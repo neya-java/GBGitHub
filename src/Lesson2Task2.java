16cb5b8e55d794b01886653e1b13f3494e9f495f
neya-java <sergey.arlashkin@gmail.com>
2021-02-14 21:59:16 +0300

    Creat file(class) Lesson2Task1 and Lesson2Task2

---
+++ src/Lesson2Task2.java
@@ -0,0 +1,18 @@
+public class Lesson2Task2 {
+    public static void main(String[] args) {
+        /*Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его
+            значениями 0 3 6 9 12 15 18 21;*/
+        System.out.println("Lesson2 #2");
+        int[] array2 = new int[8];
+        System.out.print("Массив array2: ");
+        int b = 0;
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = b;
+            b = b + 3;
+            System.out.print(array2[i] + " ");
+        }
+
+        System.out.println(" ");
+        System.out.println("----------------------------------------");
+    }
+}
