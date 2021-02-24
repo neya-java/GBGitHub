3f1f1e327c7d4230a540d96a81ec6ad61a3ec028
neya-java <sergey.arlashkin@gmail.com>
2021-02-24 22:12:01 +0300

    Modify file(class) Lesson5Task1

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+
 /*1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
         2. Конструктор класса должен заполнять эти поля при создании объекта.
         3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в
@@ -19,15 +21,20 @@ public class Employee {
     private final String e_mail;
     private final long phone_number;
     private final int salary;
-    private byte age;
+    private int age;
 
-    public byte getAge()
-    {
+    public int getAge() {
         return this.age;
     }
 
+    public void setAge(int yearOfBirth) {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        this.age = year - yearOfBirth;
+    }
+
+
     public Employee (String last_name, String first_name, String patronymic, String post, String e_mail,
-                     long phone_number, int salary, byte age) {
+                     long phone_number, int salary, int yearOfBirth) {
         this.last_name = last_name;
         this.first_name = first_name;
         this.patronymic = patronymic;
@@ -35,20 +42,15 @@ public class Employee {
         this.e_mail = e_mail;
         this.phone_number = phone_number;
         this.salary = salary;
-        this.age = age;
+        setAge(yearOfBirth);
+
     }
 
     void printCreateAnEmployee() {
         System.out.println("Имя: " + this.last_name + " / " + "Фамилия: " + this.first_name + " / " +
                 "Отчество: " + this.patronymic  + " / " + "Должность: " + this.post + " / " +
                 "Электронная почта: " + this.e_mail + " / " + "Телефон: " + this.phone_number + " / " +
-                "Зарплата: " + this.salary + " / " + "Возраст: " + this.age);
+                "Зарплата: " + this.salary + " / " + "Возраст: " + getAge());
         System.out.println();
     }
-
-
-
-
-
-
 }
