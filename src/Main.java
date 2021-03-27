d37deef8abeb6a29d2d8615a0e70af914f585252
neya-java <sergey.arlashkin@gmail.com>
2021-03-27 20:35:55 +0300

    Create files Lesson4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+import javax.swing.*;
+
+public class Main{
     public static void main(String[] args) {
+        new MyWindow();
+    }
+}
+
+class MyWindow  extends JFrame {
+    public MyWindow(){
+
+        setTitle("Моё окно");
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setBounds(500, 150, 1024, 768);
+
+        setVisible(true);
 
     }
 }
